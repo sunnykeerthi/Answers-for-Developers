6308482cb5857a42b7e715e80f5709248cb81530
sunnykeerthi <sunnykeerthi@gmail.com>
2022-04-05 19:54:51 +0530

    Capture response from reviews and dynamically update front end

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ const hbs = expbs.create({
       var ratingsTotal = options.reduce((sum, ratingVar) => {
         return sum + ratingVar.rating;
       }, 0);
-      return ratingsTotal / options.length;
+      return parseFloat(ratingsTotal / options.length).toFixed(2);
     },
   },
 });
